e0e372b61e06dbfe275da1d96ab0eec62b9c04f2
Jack Polgar <jack@polgar.id.au>
2016-09-06 04:18:12 +0000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,58 @@
+//
+// FCC Timestamp
+// Copyright (C) 2016 Nirix
+//
+
+var express = require('express');
+var router = express();
+
+var months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+router.get('/', function(req, res){
+  res.send('FreeCodeCamp Timestamp Microservice.');
+});
+
+router.get('/favicon.ico', function(req, res){
+  res.send('');
+});
+
+router.get('/:timestamp', function(req, res){
+  var result = {
+    "unix": null,
+    "natural": null
+  };
+
+  if (req.params.timestamp) {
+    var timestamp = decodeURI(req.params.timestamp);
+
+    if (Number(timestamp)) {
+      timestamp = timestamp * 1000;
+    }
+
+    var parsed = new Date(timestamp);
+
+    if (!isNaN(parsed.valueOf())) {
+      result.unix = parsed.valueOf() / 1000;
+      result.natural = months[parsed.getUTCMonth()] + ' ' + parsed.getUTCDate() + ', ' + parsed.getUTCFullYear();
+    }
+  }
+
+  res.json(result);
+});
+
+router.listen(process.env.PORT || 3000, function(){
+  console.log('Server listening on port', process.env.PORT);
+});
